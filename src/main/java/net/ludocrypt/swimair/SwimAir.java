a0ac916d5faced00037eb4c5dc5f288e35050340
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2020-08-20 09:52:21 -0500

    Done

---
+++ src/main/java/net/ludocrypt/swimair/SwimAir.java
@@ -0,0 +1,10 @@
+package net.ludocrypt.swimair;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SwimAir implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Up and Swimming!");
+	}
+}
